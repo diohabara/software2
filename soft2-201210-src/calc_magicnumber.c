34d9bfab1b01b3ddbef0590fce5cd9aa33d299f8
diohabara <diohabara@gmail.com>
2020-12-16 15:11:18 +0900

    fix: convert "Hello,World" into float

---
+++ soft2-201210-src/calc_magicnumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+void print_str_as_float(char* str) {
+  for (int i = 0; i < strlen(str); ++i) {
+    printf("%f\n", (float)str[i]);
+  }
+}
+
+int main() {
+  char h[100] = "Hello,World\n";
+  print_str_as_float(h);
+  return 0;
+}
